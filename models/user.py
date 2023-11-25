b230a460005255dfcbeb248e83bbfd362d0cd369
Kiwizacxs <brian.deleon2016@outlook.com>
2023-11-25 15:01:42 +0000

    update user and review

--- models/user.py
+++ models/user.py
@@ -14,8 +14,6 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        review = relationship("Review", backref="user",
-                              cascade="all, delete, delete-orphan")
     else:
         email = ""
         password = ""
