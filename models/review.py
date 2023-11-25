2d16e681c6c8a2c9b3f99b9fb7660607fb975823
Kiwizacxs <brian.deleon2016@outlook.com>
2023-11-25 14:03:34 +0000

    update review and user

--- models/review.py
+++ models/review.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, ForeignKey,  String
+from os import getenv
 
 
 class Review(BaseModel):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = "reviews"
+    if getenv("HBNB_TYPE_STORAGE", "fs") == "db":
+        place = Column(string(1024), nullable=False)
+        user_id = Column(string(60), nullable=False)
+        place = Column(string(60), nullable=False, ForeignKey("user.id"))
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
