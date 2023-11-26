79ddc2eb51ed6065cf9c4e1f44d59cb3f2fcf979
Kiwizacxs <brian.deleon2016@outlook.com>
2023-11-26 12:22:42 -0300

    test console

---
+++ tests/test_console.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
+from console import HBNBCommand
+import unittest
+from uuid import UUID
+import json
+import os
+
+class TestHBNBCommand(unittest.TestCase):
+    """"""
+    def EOF_test(self):
+        """"""
+        with patch()
