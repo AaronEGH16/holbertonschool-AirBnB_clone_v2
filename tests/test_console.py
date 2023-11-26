6f6de59860e655e0157904eb6e6b3512f282c535
Kiwizacxs <brian.delon2016@otulook.com>
2023-11-26 17:10:14 +0000

    test console

---
+++ tests/test_console.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
+from console import HBNBCommand
+import unittest
+from uuid import UUID
+import json
+import os
+
+class TestHBNBCommand(unittest.TestCase):
+    """"""
+    def EOF_test(self):
+        """"""
+        with patch('sys.stdout', new=StringIO()) as _output:
+            self.HBNB.onecmd("EOF")
+            self.assertEqual("\n", _output.getvalue())
+if __name__ == "__main__":
+    unittest.main()
