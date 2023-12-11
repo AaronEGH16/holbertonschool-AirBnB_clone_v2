b5dcdf984247996687d42392ee6b9e234e80c02f
AaronEGH16 <aezegonz@gmail.com>
2023-12-11 18:25:53 +0000

    same of 2-* but a default value

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+    This module contains a flask web app
+    that starts on 0.0.0.0:5000
+        routes:
+            /:              display "Hello HBNB!"
+            /hbnb:          display "HBNB"
+            /c/<text>:      display "C " followed by
+                            text converting "_" to spaces
+            /python/<text>: display "Python " followed by
+                            text converting "_" to spaces
+                            (default text value is "is cool")
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c(text):
+    mod_text = "".join([" " if letter == "_" else letter for letter in text])
+    return f"C {mod_text}"
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def python(text="is cool"):
+    mod_text = "".join([" " if letter == "_" else letter for letter in text])
+    return f"Python {mod_text}"
+
+
+if __name__ == '__main__':
+    """
+        The web application listen on
+        0.0.0.0, port 5000 in debug mode
+    """
+    app.run(host='0.0.0.0', port=5000, debug=True)
